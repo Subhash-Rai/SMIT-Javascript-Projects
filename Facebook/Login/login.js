b405386efe789f1888962572d7860ac6a0bad4d2
Subhash-Coderr <subhashpoorani928@gmail.com>
2026-01-23 00:07:19 +0500

    Dashboard of facebook project

---
+++ Facebook/Login/login.js
@@ -0,0 +1,87 @@
+
+// function showPassword() {
+//   const pass = document.getElementById("show");
+//   pass.type = pass.type === "password" ? "text" : "password"
+// };
+
+
+let fullName = document.getElementById("fullname");
+let email = document.getElementById("email");
+let firstPassword = document.getElementById("password");
+let confirmPassword = document.getElementById("confirmpassword");
+let loginBtn = document.getElementById("loginbtn");
+
+let users = [];
+
+const sweety = (error, title, message) => {
+  Swal.fire({
+    icon: error,
+    title: title,
+    text: message,
+  });
+};
+
+
+function login() {
+  const fullName = document.getElementById("fullname").value;
+  const email = document.getElementById("email").value;
+  const firstPassword = document.getElementById("firstpassword").value;
+  const confirmPassword = document.getElementById("confirmpassword").value;
+
+  console.log("Username:", fullName);
+  console.log("email:", email);
+  console.log("firstPassword:", firstPassword);
+  console.log("confirmPassword:", confirmPassword);
+
+  if (fullName === "" || email === "" || firstPassword === "" || confirmPassword === ""
+  ) {
+    return sweety(
+      "error",
+      "Something went wrong",
+      "Please Enter all fields!"
+    );
+
+  }
+  if (firstPassword !== confirmPassword
+  ) {
+    return sweety(
+      "error",
+      "Password Mismatch",
+      "Please Check your password again!"
+    );
+  }
+
+  if (firstPassword.length < 6 || confirmPassword.length < 6) {
+    return sweety(
+      "error",
+      "Weak Password",
+      "Password must be at least 6 characters long!",
+    );
+  }
+
+  if (!email.includes("@gmail.com")) {
+    return sweety(
+      "error",
+      "Invalid Email",
+      "Please enter a valid email address!",
+    );
+  }
+
+  const userObj = {
+    fullName: fullName,
+    email: email,
+    firstPassword: firstPassword,
+    confirmPassword: confirmPassword,
+  };
+  users.push(userObj);
+  localStorage.setItem("users", JSON.stringify(users));
+  sweety("success",
+    " Your Login",
+    "Congratulations! Login successfully!");
+
+    firstPassword.value = "";
+};
+
+loginBtn.addEventListener("click", login);
+
+
---
+++ Facebook/Register/register.js
@@ -0,0 +1,10 @@
+ console.log("javascript ===> running")
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+let password = document.getElementById("password");
+let gender = document.getElementsByName("gender");
+let signupBtn = document.getElementById("signupBtn");  
+
